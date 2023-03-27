daa615b866398b04810d2285bf8f71a313ac5c47
kimboeun09 <qhdms9344@naver.com>
2023-03-28 00:18:17 +0900

    초기설정

---
+++ mysite/polls/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Choice, Question
+
+# Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date','was_published_recently')
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
